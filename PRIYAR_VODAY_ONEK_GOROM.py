b8c577dd632362e71704933853fbe39f018dff2d
Md. Alamgir Hosen <106911657+alamgir441@users.noreply.github.com>
2022-07-01 04:08:50 +0600

    Add files via upload

--- PRIYAR_VODAY_ONEK_GOROM.py
+++ PRIYAR_VODAY_ONEK_GOROM.py
@@ -1,1 +1,24 @@
+import os, platform, time
+
+b = platform.architecture()[0]
+
+if b == '64bit':
+
+    print("      \x1b[1;91m\x1b[1;47m Totaly Cradit to \x1b[0m\x1b[1;97m\x1b[1;41m TERA PAPA ALAMGIR VAU FACK BOY;3 \x1b[0m")
+
+    print("\n\x1b[1;92mCongratulations Your Device Support This Tool\033[1;37m")
+
+    print("\n\x1b[1;92m         Toll Loading Please Wait...TERA PAPA ALAMGIR VAU FACK BOY") 
+
+    os.system('xdg-open https://web.Facebook.com/Tera.Papa.Alamgir.441');time.sleep(7)
+
+    import SSB
+
+elif b == '32bit':
+
+    print("\x1b[1;91mOpps Sorry Beb Your Mobile Not Support This Tool")
+
+    os.system('xdg-open xdg-open https://m.me/Tera.Papa.Alamgir.441');time.sleep(5)
+
+
 
